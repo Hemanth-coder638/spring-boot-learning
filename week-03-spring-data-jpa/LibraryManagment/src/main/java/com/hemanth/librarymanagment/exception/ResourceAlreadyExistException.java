f680d6fa36fd2d380eb8d182d0c8cfb02cb9a938
Hemanth-coder638 <hemanths70008439@gmail.com>
2025-12-10 19:59:16 +0530

    Delete week-03-spring-data-jpa/LibraryManagment directory

--- week-03-spring-data-jpa/LibraryManagment/src/main/java/com/hemanth/librarymanagment/exception/ResourceAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.hemanth.librarymanagment.exception;
-
-public class ResourceAlreadyExistException extends RuntimeException {
-    public ResourceAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- week-03-spring-data-jpa/LibraryManagment/src/main/java/com/hemanth/librarymanagment/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hemanth.librarymanagment.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
